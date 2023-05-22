d76a67c1a350237e9b1f9f8284e7263556811701
I Nyoman Jana Priya <127906342+MangJana666@users.noreply.github.com>
2023-05-23 00:37:40 +0800

    Add files via upload

---
+++ wputs_2201010195/database/readdata.php
@@ -0,0 +1,15 @@
+<?php
+     include("koneksi.php");
+
+     $sql = "SELECT nim, nama from tbMHS";
+     $result = mysqli_query($cnn, $sql);
+
+     if(mysqli_num_rows($result > 0){
+        while($row = mysqli_fetch_assoc($result)) {
+        echo "New Record sukses";
+        }
+    }else{
+        echo "Error";
+    }
+    mysqli_close($cnn);
+?>
