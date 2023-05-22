85a7571ebe5e50b37cebb09151cbd38e8de0cb70
I Nyoman Jana Priya <127906342+MangJana666@users.noreply.github.com>
2023-05-23 00:39:35 +0800

    Delete wputs_2201010195 directory

--- wputs_2201010195/database/koneksi.php
+++
@@ -1,4 +0,0 @@
-<?php
-    include("konfigurasi.php");
-
-    $cnn = mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME,DBPORT) or die("Error");
--- wputs_2201010195/konfigurasi.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    define("DBHOST", "localhost");
-    define("DBUSER", "root");
-    define("DBPASS", "");
-    define("DBPORT", "3306");
-    define("DBNAME", "db_mahasiswa");
